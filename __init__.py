afe7e4c17969a6167687f5fb0f5951ab153de388
A.I Joe <88169436+ai-joe-git@users.noreply.github.com>
2026-02-16 07:24:03 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -32,7 +32,7 @@ NODE_DISPLAY_NAME_MAPPINGS = {
 }
 
 # Version information
-__version__ = "1.2.1"
+__version__ = "1.2.3"
 
 # Web directory for custom UI components (if any)
 WEB_DIRECTORY = "./web"
